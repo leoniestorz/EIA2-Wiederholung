616987dafc3fe1d63dc9069d88c51eb82b2d16a3
leoni <leoni@leonielaptop.speedport.ip>
2018-01-06 14:44:36 +0100

    start aufgabe7

---
+++ Aufgabe7/interfaces_studiVZ.ts
@@ -0,0 +1,41 @@
+namespace StudiVZ {
+    interface StudentData {
+        
+        matrikel:number;
+        nachname:string;
+        name:string;
+        alter:number;
+        geschlecht:boolean;
+        kommentar:string;
+    }
+    
+    var students: StudentData[] = [];
+    var stop: boolean = false;
+
+    while (!stop) {
+        var action: string = prompt("Datensatz anlegen (n), abfragen(a) oder Programm beenden (s)\nn,a oder s eingeben");
+
+        switch (action) {
+            case "n":
+            case "N":
+                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                alert(saveData(input));
+                break;
+            case "a":
+            case "A":
+                var matrikel: number = parseInt(prompt("Eingabe Matrikelnummer"));
+                alert(queryData(matrikel));
+                break;
+            case "s":
+            case "S":
+                stop = true;
+        }
+    }
+
+    function saveData(_input: string): string {
+        return "Hier fehlt noch der richtige Code...";
+    }
+    function queryData(_matrikel: number): string {
+        return "Hier fehlt noch der richtige Code...";
+    }
+}
