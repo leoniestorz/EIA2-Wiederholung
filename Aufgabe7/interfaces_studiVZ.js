0d8df188e651cd5fd23d6bb44d30d878f64e6ef9
leoni <leoni@leonielaptop.speedport.ip>
2018-01-07 16:51:43 +0100

    zwischenstand aufgabe7

--- Aufgabe7/interfaces_studiVZ.js
+++ Aufgabe7/interfaces_studiVZ.js
@@ -7,7 +7,7 @@ var StudiVZ;
         switch (action) {
             case "n":
             case "N":
-                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 = m�nnlich oder 1 = weiblich) und Kommentar");
                 alert(saveData(input));
                 break;
             case "a":
@@ -21,10 +21,53 @@ var StudiVZ;
         }
     }
     function saveData(_input) {
-        return "Hier fehlt noch der richtige Code...";
+        var InputArray = _input.split(",");
+        //Angabe des Geschlechts - Zahlen als m�nnlich/weiblich festlegen
+        var geschlechtEingabe;
+        if (parseInt(InputArray[4]) == 0) {
+            geschlechtEingabe = "maennlich";
+        } // 0 = m�nnlich
+        else {
+            geschlechtEingabe = "weiblich";
+        } // 1 = weiblich
+        //Wenn falsche Eingaben gemacht werden ...  
+        if (parseInt(InputArray[0]) == NaN) {
+            return "Fehler: Matrikel als Zahl eingeben";
+        }
+        ;
+        //        
+        //        if (InputArray[1] != ""){
+        //           return "Fehler: Name mit Buchstaben eingeben"};
+        //        
+        //        if (InputArray[2] != ""){
+        //           return "Fehler: Nachname mit Buchstaben eingeben"};
+        if (parseInt(InputArray[3]) == NaN) {
+            return "Fehler: Alter als Zahl eingeben";
+        }
+        ;
+        if (parseInt(InputArray[4]) != 0 && parseInt(InputArray[4]) != 1) {
+            return "Fehler: Bitte als Geschlecht 0 oder 1 angeben";
+        }
+        ;
+        //        if (InputArray[5] != ""){
+        //           return "Fehler: Kommentar mit Buchstaben eingeben"};
+        var studi = {
+            matrikel: parseInt(InputArray[0]),
+            name: InputArray[1],
+            nachname: InputArray[2],
+            alter: parseInt(InputArray[3]),
+            geschlecht: parseInt(InputArray[4]) == 0,
+            kommentar: InputArray[5], };
+        //Objekt studi im Array students abspeichern
+        students.push(studi);
+        return "Ihre Daten wurden gespeichert: \n\nMatrikel:" + studi.matrikel + "\nVorname:" + studi.name + "\nName:" + studi.nachname + "\nAlter:" + studi.alter + "\nGeschlecht:" + studi.geschlecht + "\nKommentar:" + studi.kommentar;
     }
     function queryData(_matrikel) {
-        return "Hier fehlt noch der richtige Code...";
+        for (var i = 0; i < students.length; i++) {
+            if (students[i].matrikel == _matrikel) {
+                return "Folgende Daten sind unter dieser Matrikelnummer gespeichert:  \n\nMatrikel:" + students[i].matrikel + "\nVorname:" + students[i].name + "\nName:" + students[i].nachname + "\nAlter:" + students[i].alter + "\nGeschlecht:" + students[i].geschlecht + "\nKommentar:" + students[i].kommentar;
+            }
+        }
     }
 })(StudiVZ || (StudiVZ = {}));
 //# sourceMappingURL=interfaces_studiVZ.js.map
--- Aufgabe7/interfaces_studiVZ.ts
+++ Aufgabe7/interfaces_studiVZ.ts
@@ -18,7 +18,7 @@ namespace StudiVZ {
         switch (action) {
             case "n":
             case "N":
-                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 oder 1) und Kommentar");
+                var input: string = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0 = männlich oder 1 = weiblich) und Kommentar");
                 alert(saveData(input));
                 break;
             case "a":
@@ -33,9 +33,69 @@ namespace StudiVZ {
     }
 
     function saveData(_input: string): string {
-        return "Hier fehlt noch der richtige Code...";
+       
+      let InputArray: string[] = _input.split(",");  
+       
+    
+              
+//Angabe des Geschlechts - Zahlen als männlich/weiblich festlegen
+                
+       let geschlechtEingabe: string;
+        
+       if (parseInt(InputArray[4]) == 0) 
+       
+            {geschlechtEingabe = "maennlich";} // 0 = männlich
+       
+       else {geschlechtEingabe = "weiblich";} // 1 = weiblich
+        
+        
+//Wenn falsche Eingaben gemacht werden ...  
+              
+       if(parseInt(InputArray[0]) == NaN){
+        return "Fehler: Matrikel als Zahl eingeben"};
+//        
+//        if (InputArray[1] != ""){
+//           return "Fehler: Name mit Buchstaben eingeben"};
+//        
+//        if (InputArray[2] != ""){
+//           return "Fehler: Nachname mit Buchstaben eingeben"};
+        
+         if (parseInt(InputArray[3]) == NaN){
+           return "Fehler: Alter als Zahl eingeben"};
+        
+         if (parseInt(InputArray[4]) != 0 && parseInt(InputArray[4]) != 1){
+           return "Fehler: Bitte als Geschlecht 0 oder 1 angeben"};
+        
+//        if (InputArray[5] != ""){
+//           return "Fehler: Kommentar mit Buchstaben eingeben"};
+        
+         let studi : StudentData = {
+            
+            matrikel : parseInt(InputArray[0]),
+            name : InputArray[1],
+            nachname : InputArray [2],
+            alter : parseInt(InputArray[3]), 
+            geschlecht : parseInt(InputArray[4]) == 0,
+            kommentar : InputArray [5],}
+
+//Objekt studi im Array students abspeichern
+        
+       students.push(studi);
+        
+        
+        return "Ihre Daten wurden gespeichert: \n\nMatrikel:"+ studi.matrikel + "\nVorname:" + studi.name + "\nName:"  + studi.nachname + "\nAlter:"  + studi.alter + "\nGeschlecht:" + studi.geschlecht +"\nKommentar:" +studi.kommentar;
+        
+        
+        
+        
     }
+    
     function queryData(_matrikel: number): string {
-        return "Hier fehlt noch der richtige Code...";
-    }
+        
+        for (let i: number = 0; i < students.length; i++) {
+            
+            if (students[i].matrikel== _matrikel){
+        
+        return "Folgende Daten sind unter dieser Matrikelnummer gespeichert:  \n\nMatrikel:"+ students[i].matrikel + "\nVorname:" + students[i].name + "\nName:"  + students[i].nachname + "\nAlter:"  + students[i].alter + "\nGeschlecht:" + students[i].geschlecht +"\nKommentar:" +students[i].kommentar;
+    }}}
 }
